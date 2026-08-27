01fb9acc1e4461b8de020f903d46e3fd754a2e59
yaelalbinosantiago <yaelalbinosantiago@morfosdigital.com>
2026-08-27 13:19:52 -0600

    Metodos para editar y eliminar una tarea

--- API/Controllers/TareasController.cs
+++ API/Controllers/TareasController.cs
@@ -39,5 +39,41 @@ namespace API.Controllers
             await _db.SaveChangesAsync();
             return Ok(tarea);
         }
+
+        [HttpPut("EditarTarea/{id}")]
+        public async Task<IActionResult> EditarTarea(int id, TareaDTO tareaDTO)
+        {
+            var tarea = await _db.Tareas.FindAsync(id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            tarea.Nombre = tareaDTO.Nombre;
+            tarea.Actividad = tareaDTO.Actividad;
+            tarea.Fecha = tareaDTO.Fecha;
+            tarea.Tiempo = tareaDTO.Tiempo;
+            tarea.HoraInicio = tareaDTO.HoraInicio;
+            tarea.HoraFin = tareaDTO.HoraFin;
+
+            await _db.SaveChangesAsync();
+            return Ok(tarea);
+        }
+
+        [HttpDelete("EliminarTarea/{id}")]
+        public async Task<IActionResult> EliminarTarea(int id)
+        {
+            var tarea = await _db.Tareas.FindAsync(id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            _db.Tareas.Remove(tarea);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
